d58deebba3d671cf48ad7967c2d14adb7ec87ee8
Shuhao Zhang <jeremyz0903@gmail.com>
2023-01-25 06:38:03 +0800

    add convert api

--- app.py
+++ app.py
@@ -6,15 +6,6 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-# def city(name):
-#     df = pd.read_csv("worldcities.csv")
-#     df = df[df['city_ascii'].str.contains(name, na=False)]
-#     if df.empty:
-#         return "Not Find Result"
-#     else:
-#         return (df['city'].values[0] + " is located at (latitude, longitude) " + str(df['lat'].values[0]) + ", " + str(df['lng'].values[0])
-#               + ". It is in " + df['country'].values[0] + ". " + df['city'].values[0] + " has the population of "
-#               + str(df['population'].values[0]) + ".")
 @app.route("/<number>")
 def convert(number):
     return "The binary number is: " + str(bin(number).replace("0b", "")) + ".\n" + "The hex number is: " + str(hex(number).replace("0x", ""))
