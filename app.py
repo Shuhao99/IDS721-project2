93c6dd5d462a9677966ddb388eee52aa00286cb0
EC2 Default User <ec2-user@ip-172-31-70-68.ec2.internal>
2023-02-01 08:05:06 +0000

    week2

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, flash, redirect
 import pandas as pd
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=('GET', 'POST'))
 def hello():
-    return "Hey, It's Shuhao's server!"
+    if request.method == 'POST':
+        print("post success")
+        number_ = request.form['number']
+        print(number_)
+        return redirect(url_for('convert',number=number_))
+    else:
+        return render_template('index.html')
 
-@app.route("/try/<number>")
-def convert(number):
-    return "The binary number is: " + str(bin(int(number)).replace("0b", "")) + "." + "The hex number is: " + str(hex(int(number)).replace("0x", "")) + "."
+@app.route("/try/<number>", methods=('GET', 'POST'))
+def convert(number): 
+    if request.method == 'POST':
+        number = request.form['number']
+        return redirect(url_for('convert',number=number))
+    else:
+        print("redirect success")
+        Hex = str(hex(int(number)).replace("0x", ""))
+        binary = str(bin(int(number)).replace("0b", ""))
+        return render_template('index_check.html', Hex = Hex, Binary = binary)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port = 5000)
